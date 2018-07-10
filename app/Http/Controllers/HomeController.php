6d6da914b48359555463143e59b0537aeabd1ecb
SR512 <itplanet99@gmail.com>
2018-07-10 01:20:30 -0700

    Complete CRUD OPreation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin\User_Model;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('Admin_VW.admin');
+        $users = User_Model::paginate(10);
+        return view('Admin_VW.admin',compact('users'));
     }
 }
--- resources/views/Admin_VW/admin.blade.php
+++ resources/views/Admin_VW/admin.blade.php
@@ -17,7 +17,7 @@
                 <div class="form-group">
 
                     <div class="col-md-4{{ $errors->has('First-Name') ? ' has-error' : '' }}">
-                        <input type="text" class="form-control" name="First-Name"  placeholder="First Name">
+                        <input type="text" class="form-control" name="First-Name" placeholder="First Name">
                         @if ($errors->has('First-Name'))
                             <span class="help-block">
                                         <strong>{{ $errors->first('First-Name') }}</strong>
@@ -26,7 +26,7 @@
                     </div>
 
                     <div class="col-md-4 {{ $errors->has('Middel-Name') ? ' has-error' : '' }}">
-                        <input type="text" class="form-control" name="Middel-Name"  placeholder="Middel Name">
+                        <input type="text" class="form-control" name="Middel-Name" placeholder="Middel Name">
                         @if ($errors->has('Middel-Name'))
                             <span class="help-block">
                                         <strong>{{ $errors->first('Middel-Name') }}</strong>
@@ -35,7 +35,7 @@
                     </div>
 
                     <div class="col-md-4 {{ $errors->has('Last-Name') ? ' has-error' : '' }}">
-                        <input type="text" class="form-control" name="Last-Name"  placeholder="Last Name">
+                        <input type="text" class="form-control" name="Last-Name" placeholder="Last Name">
                         @if ($errors->has('Last-Name'))
                             <span class="help-block">
                                         <strong>{{ $errors->first('Last-Name') }}</strong>
@@ -56,18 +56,18 @@
                         @endif
                     </div>
 
-                    <div class="col-md-4 {{ $errors->has('Mobile-Number') ? ' has-error' : '' }}">
-                        <input type="text" class="form-control" name="Mobile-Number"
+                    <div class="col-md-4 {{ $errors->has('Mobile_Number') ? ' has-error' : '' }}">
+                        <input type="text" class="form-control" name="Mobile_Number"
                                placeholder="Mobile Number">
-                        @if ($errors->has('Mobile-Number'))
+                        @if ($errors->has('Mobile_Number'))
                             <span class="help-block">
-                                        <strong>{{ $errors->first('Mobile-Number') }}</strong>
+                                        <strong>{{ $errors->first('Mobile_Number') }}</strong>
                                     </span>
                         @endif
                     </div>
 
                     <div class="col-md-4 {{ $errors->has('Occupation') ? ' has-error' : '' }}">
-                        <input type="text" class="form-control" name="Occupation"  placeholder="Occupation">
+                        <input type="text" class="form-control" name="Occupation" placeholder="Occupation">
                         @if ($errors->has('Occupation'))
                             <span class="help-block">
                                         <strong>{{ $errors->first('Occupation') }}</strong>
@@ -79,7 +79,7 @@
                 <div class="form-group">
 
                     <div class="col-md-4 {{ $errors->has('Area') ? ' has-error' : '' }}">
-                        <input type="text" class="form-control" name="Area"  placeholder="Area">
+                        <input type="text" class="form-control" name="Area" placeholder="Area">
                         @if ($errors->has('Area'))
                             <span class="help-block">
                                         <strong>{{ $errors->first('Area') }}</strong>
@@ -88,7 +88,7 @@
                     </div>
 
                     <div class="col-md-4 {{ $errors->has('City') ? ' has-error' : '' }}">
-                        <input type="text" class="form-control" name="City"  placeholder="City">
+                        <input type="text" class="form-control" name="City" placeholder="City">
                         @if ($errors->has('City'))
                             <span class="help-block">
                                         <strong>{{ $errors->first('City') }}</strong>
@@ -97,7 +97,7 @@
                     </div>
 
                     <div class="col-md-4 {{ $errors->has('State') ? ' has-error' : '' }}">
-                        <input type="text" class="form-control" name="State"  placeholder="State">
+                        <input type="text" class="form-control" name="State" placeholder="State">
                         @if ($errors->has('State'))
                             <span class="help-block">
                                         <strong>{{ $errors->first('State') }}</strong>
@@ -113,7 +113,197 @@
                 <button class="btn btn-success" type="submit">Save Data</button>
 
             </div>
+
             {!! Form::close() !!}
+        </div>
+
+        <div class="white-box">
+            <table class="table table-striped" id="table">
+                <thead>
+                <tr>
+                    <th>Register</th>
+                    <th>First_Name</th>
+                    <th>Middel_Name</th>
+                    <th>Last_Name</th>
+                    <th>Mobile-Number</th>
+                    <th>Occupation</th>
+                    <th>Address</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    <tr class="item{{$user['id']}}">
+                        <td>{{$user['Register_Number']}}</td>
+                        <td>{{$user['First_Name']}}</td>
+                        <td>{{$user['Middel_Name']}}</td>
+                        <td>{{$user['Last_Name']}}</td>
+                        <td>{{$user['Mobile_Number']}}</td>
+                        <td>{{$user['Occupation']}}</td>
+                        <td><textarea disabled>{{$user['Area']}} {{$user['City']}} {{$user['State']}}</textarea></td>
+                        @if($user['Status'] == 1)
+                            <td><label class="btn btn-success">Active</label></td>
+                        @else
+                            <td><label class="btn btn-danger">Block</label></td>
+                        @endif
+                        <td>
+                            <button class="balance-modal btn btn-primary btn-sm"
+                                    data-id="{{$user['id']}}"
+                                    data-register="{{$user['Register_Number']}}"
+                                    data-first="{{$user['First_Name']}}"
+                                    data-middel="{{$user['Middel_Name']}}"
+                                    data-last="{{$user['Last_Name']}}"
+                                    data-mobile="{{$user['Mobile_Number']}}"
+                                    data-occupation="{{$user['Occupation']}}"
+                                    data-area="{{$user['Area']}}"
+                                    data-city="{{$user['City']}}"
+                                    data-state="{{$user['State']}}"
+                                    data-status="{{$user['Status']}}"
+                            >
+                                <span class='glyphicon glyphicon-pencil'></span>
+                            </button>
+                            <button class="delete-modal btn btn-danger btn-sm"
+                                    data-id="{{$user['id']}}"
+                                    data-register="{{$user['Register_Number']}}"
+                                    data-first="{{$user['First_Name']}}"
+                                    data-middel="{{$user['Middel_Name']}}"
+                                    data-last="{{$user['Last_Name']}}"
+                            >
+                                <span class='glyphicon glyphicon-trash'></span>
+                            </button>
+                            @if($user['Status'] == 1)
+                                <a class="btn btn-danger btn-sm" href="{{ route('User.edit', $user['id']) }}"
+                                >Block
+                                </a>
+                            @else
+                                <a href="{{ route('User.edit', $user['id']) }}" class="btn btn-success btn-sm">Active
+                                </a>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            <ul class="pagination">
+                @if($users->lastPage() > 1)
+                    @for($i=1 ;$i<=$users->lastpage();$i++)
+                        <li class="page-item"><a class="page-link" href="{{$users->url($i)}}">{{$i}}</a></li>
+                    @endfor
+                @endif
+            </ul>
+        </div>
+    </div>
+    </div>
+
+
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"></h4>
+                </div>
+
+                <div class="modal-body">
+
+                    {!! Form::open(['method' => 'put','class' => 'form-horizontal','form-material','id' => 'formBalance']) !!}
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+
+                        <input type="hidden" class="form-control" id="eid" name="eid">
+                        <input type="hidden" class="form-control" id="eStatus" name="eStatus">
+
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" id="eFirst-Name" name="eFirst-Name" placeholder="First Name">
+                        </div>
+
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" id="eMiddel-Name" name="eMiddel-Name" placeholder="Middel Name">
+                        </div>
+
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" id="eLast-Name" name="eLast-Name" placeholder="Last Name">
+                        </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" id="eRegister_Number" name="eRegister_Number"
+                                   placeholder="Register Number">
+                        </div>
+
+                        <div class="col-md-4 ">
+                            <input type="text" class="form-control" id="eMobile_Number" name="eMobile_Number"
+                                   placeholder="Mobile Number">
+                        </div>
+
+                        <div class="col-md-4 ">
+                            <input type="text" class="form-control" id="eOccupation" name="eOccupation" placeholder="Occupation">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+
+                        <div class="col-md-4 ">
+                            <input type="text" class="form-control" id="eArea" name="eArea" placeholder="Area">
+                        </div>
+
+                        <div class="col-md-4 ">
+                            <input type="text" class="form-control" id="eCity" name="eCity" placeholder="City">
+                        </div>
+
+                        <div class="col-md-4 ">
+                            <input type="text" class="form-control" id="eState" name="eState" placeholder="State">
+                        </div>
+
+
+                    </div>
+
+                    </form>
+
+                    <div class="enableContent">
+                        Are You Sure Want to&nbsp;<span class="title"></span>&nbsp;Account.. ?
+                        <span class="hidden id"></span>
+                        <span class="hidden href"></span>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn actionBtn" data-dismiss="modal">
+                        <span id="footer__action_button" class="glyphicon"></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--Form Edit Delete Modal--}}
+
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"></h4>
+                </div>
+                <div class="modal-body">
+
+                    <div class="deleteContent">
+                        Are You Sure Want to Delete <span class="title"></span> ?
+
+                        <span class="hidden id"></span>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn actionBtn" data-dismiss="modal">
+                        <span id="footer__action_button" class="glyphicon"></span>
+                    </button>
+
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
